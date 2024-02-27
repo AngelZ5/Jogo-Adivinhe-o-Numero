fec9b2cb23a71f4d8c3487419579d7d0f99bf070
Angel silva <angelsilvadesantana@gmail.com>
2024-02-27 01:34:28 -0300

    Nivel 2 adicionado

--- Adivinhe-o-numero.py
+++ Adivinhe-o-numero.py
@@ -20,9 +20,39 @@ if Iniciador == "s":
     print("Você tem 10 chances")
     while Chances > 0:
         SuaResposta = int(input(""))
+        vitoria = SuaResposta == Resposta
         if SuaResposta == Resposta:
             print("Você ganhou!, parabéns.")
-            break
+            chances = 10
+            print("Gostaria de ir para a segunda fase? S/N")
+            SegundoIniciador = input("").lower()
+            if SegundoIniciador == "s":
+                print("A segunda fase vai iniciar em...")
+                time.sleep(0.7)
+                print("3")
+                time.sleep(0.7)
+                print("2")
+                time.sleep(0.7)
+                print("1")
+                Resposta2 = random.randint(1, 100)
+                Chances2 = 10
+                print("Adivinhe o número entre 1 e 100")
+                while Chances2 > 0:
+                    SuaResposta2 = int(input(""))
+                    if SuaResposta2 == Resposta2:
+                        print("Você ganhou na segunda fase!, parabéns.")
+                        break
+                    elif SuaResposta2 < Resposta2:
+                        print("Chute mais alto")
+                    elif SuaResposta2 > Resposta2:
+                        print("Chute mais baixo")
+                    Chances2 -= 1
+                if Chances2 == 0:
+                    print("FIM DE JOGO")
+                break
+            else:
+                break
+
         elif SuaResposta < Resposta:
             print("Chute mais alto")
             Chances -= 1
@@ -36,3 +66,4 @@ if Iniciador == "s":
 elif Iniciador == "n":
     print("Fechando jogo")
     time.sleep(1.5)
+
